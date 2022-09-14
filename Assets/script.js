08dfeaea6525f9904b99f54b8265e366d542c4e7
sam song <samuelsong18@gmail.com>
2022-09-14 15:11:57 -0400

    added const and is preparing to start functions

--- Assets/script.js
+++ Assets/script.js
@@ -1,3 +1,12 @@
+const searchBtn = document.getElementById("searchButton");
+const APIKEY = "519ec4da1131e2d9d1d93ae1745eec7e";
+const cityEl = document.getElementById("enterCity");
+const clearBtn = document.getElementById("clearHistory");
+const weatherPicEl = document.getElementById("weatherPic");
+const tempEl = document.getElementById("temp");
+const humidityEl = document.getElementById("humidity");
+const windSpeedEl = document.getElementById("windSpeed");
+const historyEl = document.getElementById("history");
 function city(cityName) {
 fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=5&appid=519ec4da1131e2d9d1d93ae1745eec7e`)
 .then((res)=>{
@@ -8,16 +17,9 @@ fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=5&appid
 })
 
 }
-// add event listeer 
+searchBtn.addEventListener("click", city);
 // function lat
-fetch("https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&units=imperial&appid=519ec4da1131e2d9d1d93ae1745eec7e")
-.then((res)=>{
-    return res.json()
-})
-.then((data)=>{
-    console.log(data);
-})
-fetch("https://api.openweathermap.org/data/2.5/onecall?lat=39.2908816&lon=-76.610759&units=imperial&exclude=minutely,hourly&appid=519ec4da1131e2d9d1d93ae1745eec7e")
+fetch("https://api.openweathermap.org/data/2.5/weather?lat=34.1206564&lon=-84.0043513&units=imperial&appid=519ec4da1131e2d9d1d93ae1745eec7e")
 .then((res)=>{
     return res.json()
 })
